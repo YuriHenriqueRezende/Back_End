8c1990042e3af6d33758b55c6d788b7080dc12f7
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-09-21 00:10:04 -0300

    commit

--- desafio_1/desafio/views.py
+++ desafio_1/desafio/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-#a
+# Create your views here.from .models import *
+from .serializers import *
+from rest_framework.views import APIView # Manual #
+from rest_framework.viewsets import ModelViewSet # Automatico #
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+
