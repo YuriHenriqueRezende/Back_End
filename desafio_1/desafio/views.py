ee0b498cf82465cbaea5c09c613244bfe16423ee
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-09-24 00:42:08 -0300

    commit

--- desafio_1/desafio/views.py
+++ desafio_1/desafio/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-# Create your views here.from .models import *
 from .serializers import *
 from rest_framework.views import APIView # Manual #
 from rest_framework.viewsets import ModelViewSet # Automatico #
@@ -9,3 +6,37 @@ from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 
+class usuarioAPIView(APIView):
+    def get(self, request, usuarioId = ''):
+        if usuarioId == '':
+            usuarioFould = ''
+            if 'nome' in request.GET:
+                usuarioFould = usuario.objects.filter(nome__gt=request.GET['nome'])
+#            else:
+#                usuarioFould = usuario.objects.all()
+
+            usuarioSerializerd = usuarioSerializer(usuarioFould, many=True)
+            return Response(usuarioSerializerd.data)
+        else:
+            try:
+                usuarioFould = usuario.objects.get(id=usuarioId)
+                usuarioFould = usuarioSerializer(usuarioFould, many=False)
+                return Response(usuarioSerializerd.data)
+            except usuario.DoesNotExist:
+                return Response(status=404, data='People Not Found')
+
+'''
+class registroPIView(APIView):
+    def get(self, request, registroId = ''):
+        if registroId == '':
+            registroFound = registro.objects.all()
+            registroSerialized = registroSerializer(registroFound, many=True)
+            return Response(registroSerialized.data)
+        else:
+            try:
+                registroFound = registro.objects.get(id=registroId)
+                registroSerialized = registroSerializer(registroFound, many=False)
+                return Response(registroSerialized.data)
+            except:
+                return Response(status=404, data='Usuario Not Found!')
+'''
