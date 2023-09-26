1349bf2172fb7ed51415a7ea81701fe54d023055
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-09-26 19:35:33 -0300

    commit

--- Rick_And_Mory/aula/settings.py
+++ Rick_And_Mory/aula/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'aula.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'root',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306',
     }
 }
 
