0ae5987db5f0c1023ce9c01ea04ba7116c875e6e
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-09-27 00:18:52 -0300

    commit

--- desafio_2/desafio/admin.py
+++ desafio_2/desafio/admin.py
@@ -2,3 +2,37 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+# Register your models here.
+class admin_cadastro(admin.ModelAdmin):
+    list_display = ('id', 'nome','CPF','email','cep','telefone')
+    list_display_links = ('id','nome')
+    search_fields = ['nome']
+    list_per_page = 10
+
+admin.site.register(cadastro,admin_cadastro)
+
+
+class admin_categoria(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    list_display_links = ('id','nome')
+    search_fields = ['nome']
+    list_per_page = 10
+
+admin.site.register(categoria,admin_categoria)
+
+
+class admin_viagem(admin.ModelAdmin):
+    list_display = ('id', 'titulo','imagem','descrição','valor','endereço','cidade','categoria')
+    list_display_links = ('id','titulo')
+    search_fields = ['titulo']
+    list_per_page = 10
+
+admin.site.register(viagens,admin_viagem)
+
+class admin_cadastro_viagem(admin.ModelAdmin):
+    list_display = ('id', 'cadastro','viagem','data_do_inicio','data_do_fim','valor_final')
+    list_display_links = ('id','cadastro')
+    search_fields = ['cadastro']
+    list_per_page = 10
+
+admin.site.register(cadastro_viagem_usuario,admin_cadastro_viagem)
--- desafio_2/desafio/models.py
+++ desafio_2/desafio/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class cadastro(models.Model):
+    nome = models.CharField(max_length=150, null=False)
+    CPF = models.IntegerField()
+    email = models.CharField(max_length=150, null=True)
+    cep = models.IntegerField()
+    telefone = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+    
+
+class categoria(models.Model):
+    nome = models.CharField(max_length=150, null=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class viagens(models.Model):
+    titulo = models.CharField(max_length=150, null=False)
+    imagem = models.CharField(max_length=150, null=True)
+    descrição = models.TextField()
+    valor = models.IntegerField()
+    endereço = models.CharField(max_length=150, null=False)
+    cidade = models.CharField(max_length=150, null=False)
+    categoria = models.ForeignKey(categoria, related_name="categoria", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+
+class cadastro_viagem_usuario(models.Model):
+    cadastro = models.ForeignKey(cadastro, related_name="cadastro", on_delete=models.CASCADE)
+    viagem = models.ForeignKey(viagens, related_name="viagens", on_delete=models.CASCADE)
+    data_do_inicio = models.DateField(auto_now_add=False)
+    data_do_fim = models.DateField(auto_now_add=False)
+    def valor_final(self):
+        contagem = self.data_do_inicio + self.data_do_fim
+        return contagem + 150
+    
+    def __str__(self):
+        return self.cadastro
+
+
+
