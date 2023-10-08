cb2f68e804adebf1ba21dc677d65249a06c9bf39
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-10-08 00:18:49 -0300

    commit

--- Somativa_1/mange_automotive/views.py
+++ Somativa_1/mange_automotive/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.from .models import *
+from .serializers import *
+from rest_framework.views import APIView # Manual #
+from rest_framework.viewsets import ModelViewSet # Automatico #
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+
+
