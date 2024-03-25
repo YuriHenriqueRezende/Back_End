817d1bf39cca276952af3fb72ee8ac08fa352dd8
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-03-24 23:53:37 -0300

    commit

--- PROJETO_SGAE/manage.py
+++ PROJETO_SGAE/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sge_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sge.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- PROJETO_SGAE/sge/urls.py
+++ PROJETO_SGAE/sge/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sge_backend project.
+URL configuration for sge project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
-    #path('api/auth/', include('djoser.urls.jwt')),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
